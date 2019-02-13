2853b103f2c645e79f233ea7dc937f537b839038
Reva Baumann <rmbaumann@my365.bellevue.edu>
2019-02-12 20:42:27 -0600

    Create baumann-discussion-1.1.js

---
+++ week-1/baumann-discussion-1.1.js
@@ -0,0 +1,32 @@
+/*============================================; 
+Title: Discussion 1.1;
+Author: Reva Baumann; 
+Date: 07 February 2019; 
+Modified By: Reva Baumann; 
+Description: Create a simple JavaScript program;
+using values and variables with at least two errors.;
+Created simple mathematical problem.;
+===========================================*/
+*/
+
+//start program
+
+/*
+Expected Output: ;
+
+Simple algebraic problem using variables for constants;
+*/
+
+// variable declaration
+
+    var x, y, z; // Declare 3 variables
+    var x = 1 // Assign value 1 to x
+    var y = 2; // Assign value 2 to y
+
+    // calculate sum of equation
+
+    var z = x + y; // Assign the sum of the x and y to z
+
+// output
+console.log "z = x + y ".";
+
