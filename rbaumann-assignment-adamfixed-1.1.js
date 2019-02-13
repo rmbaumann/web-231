61540581ce783324bb1285f76116bbcce075f0b8
Reva Baumann <rmbaumann@my365.bellevue.edu>
2019-02-12 20:40:14 -0600

    Added original files to revised repository

---
+++ rbaumann-assignment-adamfixed-1.1.js
@@ -0,0 +1,37 @@
+
+/*
+============================================
+; Title: donner-assignment-1.1.js
+; Author: Adam Donner
+; Date: 10 February 2019
+; Modified by: Reva Baumann
+; Description: This program demonstrates the
+; use of JavaScript values and variables in
+; the of an application
+;===========================================
+*/ 
+
+// start program
+
+/*
+    Expected Output:
+
+    Addition of Numbers
+    Subtraction of Numbers
+    Multiplication of Numbers
+    Division of Numbers
+*/
+
+// Variable declaration and assignments along with the math for the application:
+const number1 = 6; 
+const number2 = 3;
+const addition = number1 + number2;
+const subtraction = number1-number2;
+const multiplication = number1*number2;
+const division = number1/number2;
+
+// output
+console.log ('\n','6 + 3 = ' + addition, '\n','6 - 3 = ' + subtraction, 
+'\n','6 * 3 = ' + multiplication, '\n','6 / 3 = ' + division)
+
+// end program
