23c167a77d7d247faf9997afa935aa8ecca4488d
Reva Baumann <rmbaumann@my365.bellevue.edu>
2019-02-16 11:16:20 -0600

    Added

--- variables-values-output.js
+++
@@ -1,8 +0,0 @@
-/*============================================; 
-Title: Assignment 1.4; 
-Author: Professor Krasso ; 
-Date: 25 June 2017; 
-Modified By: Reva Baumann; 
-Description: Variables, Values and Outputs Exercise;
-===========================================*/
-
