ceeac6892ae80564294509b9883ddb7549b66496
Reva Baumann <rmbaumann@my365.bellevue.edu>
2019-02-28 20:39:37 -0600

    Create baumann-discussion-4.1.js

---
+++ week-4/baumann-discussion-4.1.js
@@ -0,0 +1,38 @@
+/*
+============================================
+; Title: baumann-discussion-4.1.js
+; Author: Reva Baumann 
+; Date:   27 February 2019
+; Modified By: Reva Baumann
+; Description: Simple JavaScript program that uses an array with two (2) problems
+;===========================================
+*/
+
+/*
+    Expected output:
+    FirstName LastName
+    Discussion 4.1
+    Today's Date
+
+    // Expected output: Program that uses arrays with at least 2 errors
+    
+*/
+
+// lists details of formatted header, including first name, last name and assignment
+
+const header = require('./baumann-header.js');
+console.log(header.display("Reva", "Baumann", "Discussion 4.1"));
+
+// start program
+
+var animals = new Array("cat", "dog", "hedgehog", "fox); // declare an array value under four (4) elements
+
+var somValue = animals[2]; // returns the third element in a 0 indexed array
+
+console.log(someValue); // if someValue has a value of undefined, then it defaults to object
+
+var someArray = new Array ("bear"); // add new element of "bear"
+
+var someOtherArray = newArray ("wolf"); // add new element of "wolf"
+
+// end program
